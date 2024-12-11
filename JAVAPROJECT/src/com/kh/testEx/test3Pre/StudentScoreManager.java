84a59cb227bfc0bce7e63b235b79d03186db02d3
smdh1458 <smdh1458@hanmail.net>
2024-12-11 10:52:19 +0900

    객체 / 배열 / ArrayList / Set / Map

--- JAVAPROJECT/src/com/kh/testEx/test3Pre/StudentScoreManager.java
+++ JAVAPROJECT/src/com/kh/testEx/test3Pre/StudentScoreManager.java
@@ -12,48 +12,53 @@ public class StudentScoreManager {
 
         //데이터 입력
         studentScores.put("김철수", 85);
-       studentScores.put("이영희", 92);
-       studentScores.put("박민수", 77);
-       studentScores.put("최수진", 88);
+        studentScores.put("이영희", 92);
+        studentScores.put("박민수", 77);
+        studentScores.put("최수진", 88);
 
 
-       //학생 정보 출력
-       System.out.println("학생 정보 출력");
-       for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
-           System.out.println(entry.getKey() + " : " + entry.getValue()+"점");
-       }
+        //학생 정보 출력
+        System.out.println("학생 정보 출력");
+        for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue() + "점");
+        }
 
-       //합격 불합격 판단
+        //합격 불합격 판단
         System.out.println("합격 / 불합격 판단");
-       for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
-           String status  = (entry.getValue() >= 80) ? "합격" : "불합격" ;
-           System.out.println(entry.getKey()+":"+status);
-       }
+        for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
+            String status = (entry.getValue() >= 80) ? "합격" : "불합격";
+            System.out.println(entry.getKey() + ":" + status);
+        }
 
 
-       //점수 수정
+        //점수 수정
         String updateName = "박민수";
-       if (studentScores.containsKey(updateName)) {
-           studentScores.put(updateName, 82);
-           System.out.println(updateName + "의 점수를 82점으로 수정합니다.");
-       }else {
-           System.out.println("존재하지 않는 학생입니다.");
-       }
+        if (studentScores.containsKey(updateName)) {
+            studentScores.put(updateName, 82);
+            System.out.println(updateName + "의 점수를 82점으로 수정합니다.");
+        } else {
+            System.out.println("존재하지 않는 학생입니다.");
+        }
 
-       //점수 수정후 합격 불합격 판단
+        //점수 수정후 합격 불합격 판단
 
         System.out.println("점수 수정호 합격/ 불합격 판단: ");
-        ssm.printPassFailStudentScores(studentScores)
+        ssm.printPassFailStudentScores(studentScores);
 
-       //점수 출력 기능 메서드
-        /***
-         *
-         */
-        public void printPassFailStudentScores(Map<String, Integer> studentScores) {
-            for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
-                String status = (entry.getValue()>=80) ? "합격" : "불합격";
-                System.out.println(entry.getKey()+":"+status);
-            }
+
+    }
+    //점수 출력 기능 메서드
+
+    /***
+     * 학생점수로 합격 불합격 판단여부 출력하는 메서드
+     * printPassFailStudentScores(Map<String, Integer> studentScores)
+     * @param studentScores = 특정 학생들의 이름과 점수가 넘어오면 학생들의 대한 점수를 판단 후 출력
+     */
+
+    public void printPassFailStudentScores(Map<String, Integer> studentScores) {
+        for (Map.Entry<String, Integer> entry : studentScores.entrySet()) {
+            String status = (entry.getValue() >= 80) ? "합격" : "불합격";
+            System.out.println(entry.getKey() + ":" + status);
         }
     }
 }
